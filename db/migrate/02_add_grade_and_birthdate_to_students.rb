937566f0fb15ef479ee59ef417d48064b15da823
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-24 20:43:39 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,5 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-  def change
+  def AddGradeAndBirthdateToStudents
     add_column :students, :grade, :Integer
     add_column :birthdate, :grade, :String
   end
